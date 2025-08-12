8856f2e3e201564b5899a7b8fb6073d60f6325c4
Shaocheng Liu <shaocheng.liu@tum.de>
2025-08-12 12:27:44 +0200

    update button press env

--- Metaworld/metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_button_press_v2.py
+++ Metaworld/metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_button_press_v2.py
@@ -92,8 +92,12 @@ class SawyerButtonPressEnvV2(SawyerXYZEnv):
     def _set_obj_xyz(self, pos: npt.NDArray[Any]) -> None:
         qpos = self.data.qpos.flat.copy()
         qvel = self.data.qvel.flat.copy()
-        qpos[9] = pos
-        qvel[9] = 0
+        if len(qpos) == 10:
+            qpos[9] = pos
+            qvel[9] = 0
+        elif len(qpos) == 9:
+            qpos[8] = pos
+            qvel[8] = 0
         self.set_state(qpos, qvel)
 
     def reset_model(self) -> npt.NDArray[np.float64]:
