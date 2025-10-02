a9e98e7e06e509c99831e6e269481b35eeb29b37
shaocheng-liu <shaocheng.liu@tum.de>
2025-10-02 17:47:52 +0200

    sawyer

--- run.sh
+++ run.sh
@@ -329,10 +329,10 @@ mv Transformer_RNN/decision_tf_dataset/_chunk_1 Transformer_RNN/decision_tf_data
 CUDA_VISIBLE_DEVICES=2 python3 Transformer_RNN/RepresentationTransformerWithCLS.py
 
 # train col network
-CUDA_VISIBLE_DEVICES=2 python3 -u main.py setup=metaworld env=metaworld-mt1 worker.multitask.num_envs=1 experiment.mode=distill_collective_transformer transformer_collective_network.world_model.load_on_init=False
+CUDA_VISIBLE_DEVICES=2 python3 -u main.py setup=metaworld env=metaworld-mt1 worker.multitask.num_envs=1 experiment.mode=distill_collective_transformer 
 
 
-CUDA_VISIBLE_DEVICES=2 python3 -u main.py setup=metaworld env=metaworld-mt1 worker.multitask.num_envs=1 experiment.mode=train_world_model
+CUDA_VISIBLE_DEVICES=2 python3 -u main.py setup=metaworld env=metaworld-mt1 worker.multitask.num_envs=1 experiment.mode=train_world_model transformer_collective_network.world_model.load_on_init=False
 
 
 
