575548c29099126b5cf6ea9caee4831c67793232
shaocheng-liu <shaocheng.liu@tum.de>
2025-10-02 18:35:22 +0200

    1

--- temp.sh
+++ temp.sh
@@ -1,7 +1,4 @@
-CUDA_VISIBLE_DEVICES=2 python3 -u main.py setup=metaworld env=metaworld-mt1 worker.multitask.num_envs=1 experiment.mode=distill_collective_transformer
 
-mv logs/experiment_test/model_dir/model_col logs/experiment_test/model_dir/model_col_seed_4_no_tt_norm  
-
-CUDA_VISIBLE_DEVICES=2 python3 Transformer_RNN/RepresentationTransformerWithCLS.py
+CUDA_VISIBLE_DEVICES=2 python3 -u main.py setup=metaworld env=metaworld-mt1 worker.multitask.num_envs=1 experiment.mode=train_world_model transformer_collective_network.world_model.load_on_init=False
 
 CUDA_VISIBLE_DEVICES=2 python3 -u main.py setup=metaworld env=metaworld-mt1 worker.multitask.num_envs=1 experiment.mode=distill_collective_transformer 
